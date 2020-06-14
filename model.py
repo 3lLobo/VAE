be287705faf8fb73627e538461d9f2824eee147d
wolf <fwolf@deloitte.nl>
2020-06-14 19:49:40 +0200

    Unneccessary commit for a greener activity timeline.

--- model.py
+++ model.py
@@ -3,26 +3,30 @@ from tensorflow.keras import Model
 from tensorflow.keras.layers import Dense, Flatten, Conv2D
 import numpy as np
 
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+print("Num TPUs Available: ", len(tf.config.experimental.list_physical_devices('TPU')))
+
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-print(x_train[np.random.randint(11,1111)])
-# make data
+# dataset makes toubles of batches with lables (images, labels)
 train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(1000).batch(32)
 
 test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
 
-
+# vanilla CNN model: inputs an image and outputs a prediction.
 class VanillaCNN(Model):
-    def __init___(self):
-        super(VanillaCNN, self).__init___()
-        self.conv1 = Conv2D(32, 3, activation='relu')
+    def __init__(self):
+        super().__init__()
+        self.conv1 = Conv2D(28, 3, activation='relu')
         self.flatten = Flatten()
         self.d1 = Dense(128, activation='relu')
         self.d2 = Dense(10)
 
     def call(self, x):
+        x = tf.compat.v1.expand_dims(x, -1)
+        x = tf.cast(x, 'float32')
         x = self.conv1(x)
         x = self.flatten(x)
         x = self.d1(x)
@@ -30,6 +34,48 @@ class VanillaCNN(Model):
 
 # Create an instance of the model
 model = VanillaCNN()   
-model.__init___
+loss_object = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
+optimizer = tf.optimizers.Adam()
 
-pred = model.call(x_train[22])
+# for images, labels in train_ds:
+#     pred = model.call(images)
+#     print(labels, 'vs', pred)
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+@tf.function
+def train_step(images, labels):
+    with tf.GradientTape() as tape:
+        pred = model(images, training=True)
+        loss = loss_object(labels, pred)
+        grad = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grad, model.trainable_variables))
+        train_loss(loss)
+        train_accuracy(labels, pred)
+
+@tf.function
+def test_step(images, labels):
+        pred = model(images, training=False)
+        loss = loss_object(labels, pred)
+        test_loss(loss)
+        test_accuracy(labels, pred)
+EPOCHS = 11
+
+for epoch in range(EPOCHS):
+    # Reset the metrics at the start of the next epoch
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+    for images, labels in train_ds:
+        train_step(images, labels)
+    for images, labels in test_ds:
+        test_step(images, labels)
+    # Plot to console
+    template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+    print(template.format(epoch + 1,
+                    train_loss.result(),
+                    train_accuracy.result() * 100,
+                    test_loss.result(),
+                    test_accuracy.result() * 100))
