e0cd3022b488f55b04f2c8eb0a5d49662606c3e5
wolf <florian.wolf@student.uva.nl>
2020-06-23 15:06:50 +0200

    Gimme Green.

--- make_graph.py
+++ make_graph.py
@@ -1,10 +1,12 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-lollipop = nx.lollipop_graph(10, 20)
+lollipop = nx.lollipop_graph(3, 5)
 plt.subplot(121)
 nx.draw(lollipop, with_labels=True, font_weight='bold')
 plt.subplot(122)
-nx.draw_shell(lollipop, with_labels=True, font_weight='bold')
+nx.draw_spectral(lollipop, with_labels=True, font_weight='bold')
+
+
 plt.show()
 plt.savefig('graphie.png')
