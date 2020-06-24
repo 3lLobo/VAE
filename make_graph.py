17507e6ea50f252b32e2d41524beda106c23dbee
wolf <florian.wolf@student.uva.nl>
2020-06-24 12:52:34 +0200

    Lets build a GCNN with msg passing atributes.

--- make_graph.py
+++ make_graph.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-lollipop = nx.lollipop_graph(3, 5)
+lollipop = nx.lollipop_graph(2, 3)
 plt.subplot(121)
 nx.draw(lollipop, with_labels=True, font_weight='bold')
 plt.subplot(122)
@@ -10,3 +10,5 @@ nx.draw_spectral(lollipop, with_labels=True, font_weight='bold')
 
 plt.show()
 plt.savefig('graphie.png')
+
+print(nx.adjacency_matrix(lollipop))
