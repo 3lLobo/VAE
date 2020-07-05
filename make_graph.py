8da9009bab8178aa47f7da9b532a8d3881ed4275
wolf <florian.wolf@student.uva.nl>
2020-07-05 18:26:36 +0200

    Testing the max pooling.

--- make_graph.py
+++ make_graph.py
@@ -11,13 +11,22 @@ nx.draw_spectral(lollipop, with_labels=True, font_weight='bold')
 plt.show()
 plt.savefig('graphie.png')
 
-print(nx.adjacency_matrix(lollipop))
+# print(nx.adjacency_matrix(lollipop
+
 graph = lollipop
 graph.add_node(6)
 graph.add_edge(6,1)
-graph.add_attribute(6, 'a')
-print(nx.nodes(graph))
-print(nx.adjacency_matrix(graph))
-print(nx.adjacency_data(graph))
-print(nx.attr_matrix(graph))
-print(nx.attribute_mixing_matrix(graph, '3'))
+
+FG = nx.Graph()
+FG.add_weighted_edges_from([(1, 2, 0.125), (1, 3, 0.75), (2, 4, 1.2), (3, 4, 0.375)], peep='Pink')
+
+for (u, v, wt) in FG.edges.data('weight'):
+    if wt < 0.9: 
+        print('(%d, %d, %.3f)' % (u, v, wt))
+FG.nodes[1]['peep'] = 'Poop'      
+FG.add_edges_from([(1,3),(2,5)], meep='Peep')
+print(FG.nodes.data())
+print(FG.edges.data())
+
+print(FG.edge_attr_dict_factory())
+print(FG.node_attr_dict_factory())
--- max_pooling.py
+++ max_pooling.py
@@ -29,20 +29,33 @@ class MPGM():
         E_t = np.transpose(E, (2,0,1))
         F_t = np.transpose(F, (1,0))
         # TODO: Mask A and A_hat
+        A_diag_zero = self.zero_mask_diag(A)
+        A_hat_diag_zero = self.zero_mask_diag(A_hat)
+        A_hat_diag = self.zero_mask_diag(A, inverse=True)
+        S1 = np.matmul(E_t, E_hat)
+        S2 = A_diag_zero @ A_hat_diag_zero @ A_hat_diag @ A_hat_diag
+        S3 = np.matmul(np.matmul(F_t, F_hat), A_hat_diag)
+        S = np.matmul(S1, S2) + S3
         
-        S = np.matmul(np.matmul(E_t, E_hat)), (A @ A_hat @ A_hat @ A_hat) + np.matmul(np.matmul(F_t, F_hat), A_hat)
-        pass
+        return S
 
 
 if __name__ == "__main__":
+
+    # unnesseccary generation of a lollipop graph
     lollipop = nx.lollipop_graph(3, 3)
     lollipop.add_edge(3,2)
-    A = nx.to_numpy_matrix(lollipop)
-    E = nx.attr_matrix(lollipop)
-    A = np.ones_like(A)
-    print(A)
-    print(E)
 
+    # Generation of random test graphs
+    A = np.ones((6,6))
+    E = np.random.randint(2, size=(6,6,2))
+    F = np.random.randint(2, size=(6,3))
+    A_hat = np.random.randint(2, size=(6,6))
+    E_hat = np.random.randint(2, size=(6,6,2))
+    F_hat = np.random.randint(2, size=(6,3))
+
+    # Test the class, acctually this should go in a test function and folder. Later...
     mpgm = MPGM()
-    Am = mpgm.zero_mask_diag(A, inverse=True)
-    print(Am)
+
+    S = mpgm.similarity(A, A_hat, E, E_hat, F, F_hat)
+    print(S)
